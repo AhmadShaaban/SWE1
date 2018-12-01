a2d5e896b57bae400162dbdb9d468c85b132f9fd
beshomedhat <45463861+beshomedhat@users.noreply.github.com>
2018-12-01 19:11:08 +0200

    Update Main.java

--- GIT Assignment/src/Main.java
+++ GIT Assignment/src/Main.java
@@ -179,7 +179,7 @@ public class Main {
         while(i<sz)
         {
             int c=0;
-            for(int j=2; j<=9; j++)
+            for(int j=2; j<=arr[i]; j++)
             {
                 if(arr[i]%j==0 && arr[i]!=j)
                 {
